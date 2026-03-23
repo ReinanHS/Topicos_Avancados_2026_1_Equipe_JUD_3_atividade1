ce342fe434cbe74a44ed32f6bdd91d665d68b024
Reinan Souza <reinangabriel1520@gmail.com>
2026-03-22 23:28:25 -0300

    feat: Adiciona script inicial para carregar subconjuntos específicos de dados

--- src/reinan/main.py
+++ src/reinan/main.py
@@ -1,6 +1,10 @@
+from datasets import load_dataset
+
 def main():
-    from datasets import load_dataset
-    
+    load_dataset_oab_bench()
+    load_dataset_oab_exams()
+
+def load_dataset_oab_bench():
     ds_guidelines = load_dataset("maritaca-ai/oab-bench", "guidelines")
     ds_questions = load_dataset("maritaca-ai/oab-bench", "questions")
     
@@ -10,6 +14,14 @@ def main():
     print(f"Foram selecionadas {len(questions_reinan)} questões para o lote.")
     print("Conjuntos de dados carregados com sucesso!")
 
+def load_dataset_oab_exams():
+    ds_exams = load_dataset("eduagarcia/oab_exams")
+    
+    questions = list(ds_exams['train'])
+    questions_reinan = questions[1845:1967]
+
+    print(f"Foram selecionadas {len(questions_reinan)} questões para o lote.")
+    print("Conjuntos de dados carregados com sucesso!")
 
 if __name__ == "__main__":
     main()
